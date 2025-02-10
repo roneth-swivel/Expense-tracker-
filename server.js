8b29a8e9c354ea4e1242fa65b6a67fa0d71f4d61
roneth-swivel <roneth@swivelgroup.com.au>
2025-02-10 13:12:10 +0530

    Added comments and minor improvements

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocs = require("./swagger.json"); // Assuming you've added the swagger.json file
+const swaggerDocs = require("./swagger.json"); 
 
 // Load environment variables
 dotenv.config();
